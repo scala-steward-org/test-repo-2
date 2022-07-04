7071a31ab1a04c1cb176f56e155a55f0986abfed
Frank S. Thomas <frank@timepit.eu>
2022-07-04 22:04:57 +0200

    f

--- src/main/scala/Fs2Test.scala
+++ src/main/scala/Fs2Test.scala
@@ -16,11 +16,11 @@
 
 import fs2.text.utf8.decode
 
-object Fs2Test extends App {
-  fs2.Stream
+  object Fs2Test extends App {
+    fs2.Stream
     .emits("Hello".getBytes("UTF-8"))
-    .through(decode)
-    .compile
+     .through(decode)
+     .compile
      .toList
       .foreach(println)
 }
