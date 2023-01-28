638a5cd9b850950794e0053bc8fabea31a722f1c
Frank S. Thomas <frank@timepit.eu>
2023-01-28 12:20:55 +0100

    Add bazel project

--- project/plugins.sbt
+++
@@ -1,4 +0,0 @@
-addSbtPlugin("com.codecommit" % "sbt-github-actions" % "0.14.2")
-addSbtPlugin("com.github.sbt" % "sbt-ci-release" % "1.5.11")
-addSbtPlugin("de.heikoseeberger" % "sbt-header" % "5.9.0")
-addSbtPlugin("com.lightbend.sbt" % "sbt-java-formatter" % "0.8.0")
---
+++ src/main/java/com/example/myproject/App.java
@@ -0,0 +1,14 @@
+package com.example.myproject;
+
+public class App {
+
+  public static int compare(int a, int b) {
+    return 0;
+  }
+
+  public static void main(String... args) throws Exception {
+    App app = new App();
+    System.out.println("Success: " + app.compare(2, 1));
+  }
+
+}
